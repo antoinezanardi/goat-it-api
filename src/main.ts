faf7a41203ef27bf2fd8e375f25c1189d8f0db98
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2025-10-12 14:29:36 +0200

    refactor(app): simplify application bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
 import { bootstrap } from "./server/server";
 
-try {
-  void bootstrap();
-} catch (error) {
-  console.error("Error during application bootstrap:", error);
-  process.exit(1);
-}
+void bootstrap();
--- src/server/server.ts
+++ src/server/server.ts
@@ -8,11 +8,12 @@ async function bootstrap(): Promise<NestFastifyApplication> {
     new FastifyAdapter(),
   );
 
+  app.enableShutdownHooks();
 
   const host = process.env.HOST ?? '0.0.0.0';
   const port = parseInt(process.env.PORT ?? '3000', 10);
-
   await app.listen({ host, port });
+
   const appUrl = await app.getUrl();
   console.log(`🐐 Goat It API is running on: ${appUrl}`);
 
