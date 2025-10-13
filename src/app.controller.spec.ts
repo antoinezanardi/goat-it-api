75e2a8bc40529a84fbf765160bd409a2dfdd7eb3
Antoine Zanardi <antoine.zanardi@epitech.eu>
2025-10-13 18:51:29 +0200

    feat(tests): add vitest coverage configuration (#14)

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,11 @@
+import { AppController } from "./app.controller";
+
+describe("App Controller", () => {
+  describe("getHello", () => {
+    const appController = new AppController();
+
+    it("should return 'Hello World!' when called.", () => {
+      expect(appController.getHello()).toBe("Hello World!");
+    });
+  });
+});
---
+++ src/main.spec.ts
@@ -0,0 +1,15 @@
+import * as Server from "./server/server";
+
+vi.mock("./server/server", () => ({
+  bootstrap: vi.fn(),
+}));
+
+describe("Main", () => {
+  describe("Root", () => {
+    it("should call bootstrap when file is imported.", async() => {
+      await import("./main");
+
+      expect(Server.bootstrap).toHaveBeenCalledExactlyOnceWith();
+    });
+  });
+});
