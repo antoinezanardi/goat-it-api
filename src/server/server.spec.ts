4ecb6c80eb301053ea278fe6bcb97cdf3831758a
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2025-10-28 23:05:24 +0100

    feat(logger): integrate Pino logger for improved logging

--- src/server/server.spec.ts
+++ src/server/server.spec.ts
@@ -1,11 +1,12 @@
 import * as NestCore from "@nestjs/core";
 import * as Fastify from "@nestjs/platform-fastify";
+import { Logger } from "nestjs-pino";
 
 import type { AppModule } from "@app/app.module";
 
 import { bootstrap } from "@server/server";
 
-import type { INestApplication } from "@nestjs/common";
+import type { INestApplication, NestApplicationOptions } from "@nestjs/common";
 
 vi.mock(import("@nestjs/core"));
 vi.mock(import("@app/app.module"), () => ({
@@ -16,24 +17,37 @@ vi.mock(import("@app/app.module"), () => ({
 
 describe("Server", () => {
   beforeEach(() => {
-    vi.mocked(NestCore.NestFactory.create, { partial: true }).mockResolvedValue({
+    vi.mocked(NestCore.NestFactory.create, { partial: true }).mockResolvedValue(({
       enableShutdownHooks: vi.fn<() => INestApplication>(),
+      useLogger: vi.fn<() => void>(),
+      get: vi.fn<() => object>().mockReturnValue({
+        log: vi.fn<() => void>(),
+      }),
       listen: vi.fn<() => Promise<undefined>>(),
       getUrl: vi.fn<() => Promise<string>>().mockResolvedValue("http://mocked-host:9090"),
-    });
+    } as Partial<INestApplication>));
   });
 
   describe(bootstrap, () => {
     it("should create from NestFactory when called.", async() => {
+      const expectedOptions: NestApplicationOptions = {
+        bufferLogs: true,
+      };
       await bootstrap();
 
-      expect(NestCore.NestFactory.create).toHaveBeenCalledExactlyOnceWith({ name: "MockedModule" }, expect.any(Fastify.FastifyAdapter));
+      expect(NestCore.NestFactory.create).toHaveBeenCalledExactlyOnceWith({ name: "MockedModule" }, expect.any(Fastify.FastifyAdapter), expectedOptions);
     });
 
     it("should enable shutdown hooks when called.", async() => {
       const app = await bootstrap();
 
       expect(app.enableShutdownHooks).toHaveBeenCalledExactlyOnceWith();
+    });
+
+    it("should use logger when called.", async() => {
+      const app = await bootstrap();
+
+      expect(app.useLogger).toHaveBeenCalledExactlyOnceWith(app.get(Logger));
     });
 
     it("should listen on the default host and port when none are provided.", async() => {
@@ -58,16 +72,17 @@ describe("Server", () => {
     });
 
     it("should log the app url when called.", async() => {
-      const consoleLogSpy = vi.spyOn(console, "log");
-      await bootstrap();
+      const app = await bootstrap();
 
-      expect(consoleLogSpy).toHaveBeenCalledExactlyOnceWith("🐐 Goat It API is running on: http://mocked-host:9090");
+      expect(app.get(Logger).log).toHaveBeenCalledExactlyOnceWith("🐐 Goat It API is running on: http://mocked-host:9090");
     });
 
     it("should return the app when called.", async() => {
       const app = await bootstrap();
       const expectedApp = {
         enableShutdownHooks: expect.any(Function) as () => INestApplication,
+        useLogger: expect.any(Function) as () => void,
+        get: expect.any(Function) as () => never,
         listen: expect.any(Function) as () => void,
         getUrl: expect.any(Function) as () => void,
       };
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
+import { Logger } from "nestjs-pino";
 
 import { AppModule } from "@app/app.module";
 
@@ -9,9 +10,13 @@ async function bootstrap(): Promise<NestFastifyApplication> {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
 
+  const logger = app.get(Logger);
+
   app.enableShutdownHooks();
+  app.useLogger(logger);
 
   const host = process.env.HOST ?? "0.0.0.0";
   const port = Number.parseInt(process.env.PORT ?? "3000");
@@ -19,9 +24,7 @@ async function bootstrap(): Promise<NestFastifyApplication> {
 
   const appUrl = await app.getUrl();
 
-  // TODO [+winston]: Replace with proper logger (Winston) Linked issue : https://github.com/antoinezanardi/goat-it-api/issues/21
-  // eslint-disable-next-line no-console
-  console.log(`🐐 Goat It API is running on: ${appUrl}`);
+  logger.log(`🐐 Goat It API is running on: ${appUrl}`);
 
   return app;
 }
