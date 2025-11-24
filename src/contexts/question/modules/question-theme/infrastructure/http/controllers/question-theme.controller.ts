819de42de782eaa859eb43d8f02ae546db290f9a
Antoine ZANARDI <antoine.zanardi.partner@decathlon.com>
2025-11-24 15:29:32 +0100

    refactor(controllers): add ControllerPrefixes enum for route management

--- src/contexts/question/modules/question-theme/infrastructure/http/controllers/question-theme.controller.ts
+++ src/contexts/question/modules/question-theme/infrastructure/http/controllers/question-theme.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, HttpStatus } from "@nestjs/common";
 import { ZodResponse } from "nestjs-zod";
 
+import { ControllerPrefixes } from "@shared/infrastructure/http/controllers/controllers.enums";
+
 import { createQuestionThemeDtoFromEntity } from "@question/modules/question-theme/application/mappers/question-theme.dto.mappers";
 import { QuestionThemeDto } from "@question/modules/question-theme/application/dto/question-theme.dto";
 import { FindAllQuestionThemesUseCase } from "@question/modules/question-theme/application/use-cases/find-all-question-themes/find-all-question-themes.use-case";
 
-@Controller("question-themes")
+@Controller(ControllerPrefixes.QUESTION_THEMES)
 export class QuestionThemeController {
   public constructor(private readonly findAllQuestionThemesUseCase: FindAllQuestionThemesUseCase) {}
 
