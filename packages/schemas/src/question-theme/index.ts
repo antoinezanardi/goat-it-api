48adef008c502f35f64626724376d4b93c851d36
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2026-05-29 19:10:40 +0200

    feat(schema): add typecheck step and update version to 0.0.11

--- packages/schemas/src/question-theme/index.ts
+++ packages/schemas/src/question-theme/index.ts
@@ -1,23 +1,23 @@
 export {
-  QUESTION_THEME_STATUSES,
+  ADMIN_QUESTION_THEME_SORTABLE_FIELDS,
   DEFAULT_QUESTION_THEME_STATUS,
+  QUESTION_THEME_SORTABLE_FIELDS,
   QUESTION_THEME_STATUS_ARCHIVED,
+  QUESTION_THEME_STATUSES,
 } from "@question-theme/domain/constants/question-theme.constants";
 
-export { QUESTION_THEME_CREATION_DTO } from "@question-theme/application/dto/question-theme-creation/question-theme-creation.dto.shape";
-
-export { QUESTION_THEME_MODIFICATION_DTO } from "@question-theme/application/dto/question-theme-modification/question-theme-modification.dto.shape";
-
-export { QUESTION_THEME_DTO } from "@question-theme/application/dto/question-theme/question-theme.dto.shape";
-
+export { ADMIN_FIND_QUESTION_THEMES_SORT_QUERY_DTO } from "@question-theme/application/dto/admin-find-question-themes-sort-query/admin-find-question-themes-sort-query.dto.shape";
 export { ADMIN_QUESTION_THEME_DTO } from "@question-theme/application/dto/admin-question-theme/admin-question-theme.dto.shape";
+export { FIND_QUESTION_THEMES_SORT_QUERY_DTO } from "@question-theme/application/dto/find-question-themes-sort-query/find-question-themes-sort-query.dto.shape";
+export { QUESTION_THEME_CREATION_DTO } from "@question-theme/application/dto/question-theme-creation/question-theme-creation.dto.shape";
+export { QUESTION_THEME_MODIFICATION_DTO } from "@question-theme/application/dto/question-theme-modification/question-theme-modification.dto.shape";
+export { QUESTION_THEME_DTO } from "@question-theme/application/dto/question-theme/question-theme.dto.shape";
 
 export type { QuestionThemeStatus } from "@question-theme/domain/types/question-theme.value-objects";
 
-export type { QuestionThemeCreationDto } from "@question-theme/application/dto/question-theme-creation/question-theme-creation.dto.shape";
-
-export type { QuestionThemeModificationDto } from "@question-theme/application/dto/question-theme-modification/question-theme-modification.dto.shape";
-
-export type { QuestionThemeDto } from "@question-theme/application/dto/question-theme/question-theme.dto.shape";
-
+export type { AdminFindQuestionThemesSortQueryDto } from "@question-theme/application/dto/admin-find-question-themes-sort-query/admin-find-question-themes-sort-query.dto.shape";
 export type { AdminQuestionThemeDto } from "@question-theme/application/dto/admin-question-theme/admin-question-theme.dto.shape";
+export type { FindQuestionThemesSortQueryDto } from "@question-theme/application/dto/find-question-themes-sort-query/find-question-themes-sort-query.dto.shape";
+export type { QuestionThemeCreationDto } from "@question-theme/application/dto/question-theme-creation/question-theme-creation.dto.shape";
+export type { QuestionThemeModificationDto } from "@question-theme/application/dto/question-theme-modification/question-theme-modification.dto.shape";
+export type { QuestionThemeDto } from "@question-theme/application/dto/question-theme/question-theme.dto.shape";
